f85ce72f8415ce84121bb674966c05f01295d4cc
katrina-mathebula <60740241+katrina-mathebula@users.noreply.github.com>
2020-07-02 18:35:40 +0200

    updated

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ $products = mysqli_fetch_all($result,MYSQLI_ASSOC);
 
     
     <header id="header">
-    <nav class="navbar navbar-expand-lg px-4">
+    <nav class="navbar navbar-expand-lg px-4" id="nav">
         <!-- 
     https://www.iconfinder.com/icons/5296677/bamboo_branch_forest_leaf_nature_plant_tree_icon
     Creative Commons (Attribution 3.0 Unported);
@@ -66,10 +66,10 @@ $products = mysqli_fetch_all($result,MYSQLI_ASSOC);
                 <a href="#header" class="nav-link"> home</a>
                 </li>
                 <li class="nav-item active">
-                <a href="#about" class="nav-link"> about</a>
+                <a href="about.html" class="nav-link"> about</a>
                 </li>
                 <li class="nav-item active">
-                <a href="#store" class="nav-link"> store</a>
+                <a href="gallery.php" class="nav-link"> gallery</a>
                 </li>
                 <li class="nav-item active">
                 <a href="#order" class="nav-link"> order</a>
@@ -175,6 +175,45 @@ $products = mysqli_fetch_all($result,MYSQLI_ASSOC);
             ?>
             
 <!-- end of cart item -->
+                <script
+                
+                  obj.clearCart = function() {
+                    cart = [];
+                    saveCart();
+                  }
+                       
+                    $('.clear-cart').click(function() {
+                      shoppingCart.clearCart();
+                      displayCart();
+                    });
+                        
+                        // Clear items
+$('.clear-cart').click(function() {
+  shoppingCart.clearCart();
+  displayCart();
+});
+
+
+function displayCart() {
+  var cartArray = shoppingCart.listCart();
+  var output = "";
+  for(var i in cartArray) {
+    output += "<tr>"
+      + "<td>" + cartArray[i].name + "</td>" 
+      + "<td>(" + cartArray[i].price + ")</td>"
+      + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+      + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+      + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
+      + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+      + " = " 
+      + "<td>" + cartArray[i].total + "</td>" 
+      +  "</tr>";
+  }
+  $('.show-cart').html(output);
+  $('.total-cart').html(shoppingCart.totalCart());
+  $('.total-count').html(shoppingCart.totalCount());
+}
+                        </script>
 
                 
                 
